a1688a9254364b207d8433b2c941144bea074e1e
brandonmcghee <brandonmcghee@fullsail.edu>
2013-05-06 20:56:50 -0400

    First Commit
    Project Initialization

---
+++ AOC1305Week1/AOC1305Week1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AOC1305Week1
+//
+//  Created by Brandon McGhee on 5/6/13.
+//  Copyright (c) 2013 Brandon McGhee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AOC1305Week1/AOC1305Week1/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  AOC1305Week1
+//
+//  Created by Brandon McGhee on 5/6/13.
+//  Copyright (c) 2013 Brandon McGhee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+    
+    NSLog(@"testing");
+}
