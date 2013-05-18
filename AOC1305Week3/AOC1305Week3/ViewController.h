b3efad11d14d3031b5886816e0e13a53a55b9eb8
brandonmcghee <brandonmcghee@fullsail.edu>
2013-05-18 17:47:11 -0400

    Week 3 Initial Commit
    Xcode project created for single view application.  Beginning to watch
    videos now.

---
+++ AOC1305Week3/AOC1305Week3/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AOC1305Week3
+//
+//  Created by Brandon McGhee on 5/18/13.
+//  Copyright (c) 2013 Brandon McGhee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
