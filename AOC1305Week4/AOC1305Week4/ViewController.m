2e1194a4bef20563981857a34393e98a96ed681d
brandonmcghee <brandonmcghee@fullsail.edu>
2013-05-26 13:34:41 -0400

    Week 4 Commit
    Fixing layout of buttons, also added info button

--- AOC1305Week4/AOC1305Week4/ViewController.m
+++ AOC1305Week4/AOC1305Week4/ViewController.m
@@ -34,7 +34,7 @@
     
     //Login Button
     UIButton *btnLogin = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-    btnLogin.frame = CGRectMake(deviceWidth/1.5, 300.0f, 100.0f, 30.0f);
+    btnLogin.frame = CGRectMake(deviceWidth/1.5, deviceHeight/10, 100.0f, 30.0f);
     [btnLogin setTitle:@"Login" forState:UIControlStateNormal];
     
     //Label for "Please Enter Username"
@@ -44,12 +44,22 @@
     labelPEName.text = @"Please Enter Username";
     labelPEName.textAlignment = NSTextAlignmentCenter;
     
+    //Show Date Button
+    UIButton *btnDate = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    btnDate.frame = CGRectMake(10.0f, 300.0f, 100.0f, 30.0f);
+    [btnDate setTitle:@"Show Date" forState:UIControlStateNormal];
+    
+    UIButton *btnInfo = [UIButton buttonWithType:UIButtonTypeInfoDark];
+    btnInfo.frame = CGRectMake(10.0f, deviceHeight/1.5, 25.0f, 100.0f);
+    
     self.view.backgroundColor = [UIColor whiteColor];
     //Adding elements to the view to be displayed
     [self.view addSubview:labelName];
     [self.view addSubview:inputName];
     [self.view addSubview:btnLogin];
     [self.view addSubview:labelPEName];
+    [self.view addSubview:btnDate];
+    [self.view addSubview:btnInfo];
 
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
