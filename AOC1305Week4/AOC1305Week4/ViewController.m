3d917725c49039f2507b0bfd89d173ab0268e9a5
brandonmcghee <brandonmcghee@fullsail.edu>
2013-05-26 13:54:26 -0400

    Week 4 Commit
    Layout finished, everything looks to be in order, continuing with the
    rest of the assignment

--- AOC1305Week4/AOC1305Week4/ViewController.m
+++ AOC1305Week4/AOC1305Week4/ViewController.m
@@ -34,11 +34,11 @@
     
     //Login Button
     UIButton *btnLogin = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-    btnLogin.frame = CGRectMake(deviceWidth/1.5, deviceHeight/10, 100.0f, 30.0f);
+    btnLogin.frame = CGRectMake(deviceWidth/1.4, deviceHeight/10, 75.0f, 30.0f);
     [btnLogin setTitle:@"Login" forState:UIControlStateNormal];
     
     //Label for "Please Enter Username"
-    UILabel *labelPEName = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, deviceHeight/5, deviceWidth, 100.0f)];
+    UILabel *labelPEName = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, deviceHeight/5, deviceWidth, 75.0f)];
     labelPEName.backgroundColor = [UIColor grayColor];
     labelPEName.textColor = [UIColor blueColor];
     labelPEName.text = @"Please Enter Username";
@@ -46,11 +46,17 @@
     
     //Show Date Button
     UIButton *btnDate = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-    btnDate.frame = CGRectMake(10.0f, 300.0f, 100.0f, 30.0f);
+    btnDate.frame = CGRectMake(10.0f, deviceHeight/2, 100.0f, 50.0f);
     [btnDate setTitle:@"Show Date" forState:UIControlStateNormal];
     
+    //Info Button
     UIButton *btnInfo = [UIButton buttonWithType:UIButtonTypeInfoDark];
-    btnInfo.frame = CGRectMake(10.0f, deviceHeight/1.5, 25.0f, 100.0f);
+    btnInfo.frame = CGRectMake(10.0f, deviceHeight/1.6, 25.0f, 100.0f);
+    
+    //"Created by" Label
+    UILabel *labelCredits = [[UILabel alloc] initWithFrame:CGRectMake(10.0f, deviceHeight/1.25, deviceWidth, 50.0f)];
+    labelCredits.text = @"This application was created by \nBrandon McGhee";
+    labelCredits.numberOfLines = 2;
     
     self.view.backgroundColor = [UIColor whiteColor];
     //Adding elements to the view to be displayed
@@ -60,6 +66,7 @@
     [self.view addSubview:labelPEName];
     [self.view addSubview:btnDate];
     [self.view addSubview:btnInfo];
+    [self.view addSubview:labelCredits];
 
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
