fafe95cf222fe76c8f6bb582c3bf81af56df396a
brandonmcghee <brandonmcghee@fullsail.edu>
2013-05-26 12:13:44 -0400

    Week 4 Commit
    Created Username Label, Username Textfield, and a variable to detect
    screen width.

--- AOC1305Week4/AOC1305Week4/ViewController.m
+++ AOC1305Week4/AOC1305Week4/ViewController.m
@@ -16,6 +16,31 @@
 
 - (void)viewDidLoad
 {
+    //Float to capture the width of current device
+    float deviceWidth = CGRectGetWidth([[UIScreen mainScreen] bounds]);
+    
+    //Label for "Username:"
+    UILabel *labelName = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, 10.0f, deviceWidth/2, 31.0f)];
+    labelName.text = @"Username:";
+    labelName.textColor = [UIColor blackColor];
+    
+    //Text Input for "Username:" label
+    UITextField *inputName = [[UITextField alloc] initWithFrame:CGRectMake(90.0f, 10.0f, deviceWidth/1.5, 31.0f)];
+    [inputName setBorderStyle:(UITextBorderStyleRoundedRect)];
+    
+    //Label for "Please Enter Username"
+    UILabel *labelPEName = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, 150.0f, deviceWidth, 150.0f)];
+    labelPEName.backgroundColor = [UIColor grayColor];
+    labelPEName.textColor = [UIColor blueColor];
+    labelPEName.text = @"Please Enter Username";
+    labelPEName.textAlignment = NSTextAlignmentCenter;
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    //Adding elements to the view to be displayed
+    [self.view addSubview:labelName];
+    [self.view addSubview:inputName];
+    [self.view addSubview:labelPEName];
+    
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
