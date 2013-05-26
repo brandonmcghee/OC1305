41b625dfe3b23a0cd6a4d812bd7ec6ea50afa42e
brandonmcghee <brandonmcghee@fullsail.edu>
2013-05-26 13:01:15 -0400

    Week 4 Commit
    Added the login button, also created variable to detect screen height
    and apply it to the x value of labels

--- AOC1305Week4/AOC1305Week4/ViewController.m
+++ AOC1305Week4/AOC1305Week4/ViewController.m
@@ -18,6 +18,10 @@
 {
     //Float to capture the width of current device
     float deviceWidth = CGRectGetWidth([[UIScreen mainScreen] bounds]);
+    float deviceHeight = CGRectGetHeight([[UIScreen mainScreen] bounds]);
+    
+    //Date
+    NSDate *currentDate = [NSDate date];
     
     //Label for "Username:"
     UILabel *labelName = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, 10.0f, deviceWidth/2, 31.0f)];
@@ -28,8 +32,13 @@
     UITextField *inputName = [[UITextField alloc] initWithFrame:CGRectMake(90.0f, 10.0f, deviceWidth/1.5, 31.0f)];
     [inputName setBorderStyle:(UITextBorderStyleRoundedRect)];
     
+    //Login Button
+    UIButton *btnLogin = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    btnLogin.frame = CGRectMake(deviceWidth/1.5, 300.0f, 100.0f, 30.0f);
+    [btnLogin setTitle:@"Login" forState:UIControlStateNormal];
+    
     //Label for "Please Enter Username"
-    UILabel *labelPEName = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, 150.0f, deviceWidth, 150.0f)];
+    UILabel *labelPEName = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, deviceHeight/5, deviceWidth, 100.0f)];
     labelPEName.backgroundColor = [UIColor grayColor];
     labelPEName.textColor = [UIColor blueColor];
     labelPEName.text = @"Please Enter Username";
@@ -39,8 +48,9 @@
     //Adding elements to the view to be displayed
     [self.view addSubview:labelName];
     [self.view addSubview:inputName];
+    [self.view addSubview:btnLogin];
     [self.view addSubview:labelPEName];
-    
+
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
